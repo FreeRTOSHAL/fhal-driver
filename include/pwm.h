302d07f71cfb3ceba9373e1a3294f212075a927b
Werner2005 <webmaster@andy89.org>
2016-02-17 02:40:07 +0100

    change index of pwm interface to uint32_t

--- include/pwm.h
+++ include/pwm.h
@@ -9,7 +9,7 @@
 struct pwm;
 #ifdef CONFIG_PWM_MULTI
 struct pwm_ops {
-	struct pwm *(*pwm_init)(int32_t index);
+	struct pwm *(*pwm_init)(uint32_t index);
 	int32_t (*pwm_deinit)(struct pwm *pwm);
 
 	int32_t (*pwm_setPeriod)(struct pwm *pwm, uint64_t us);
@@ -24,13 +24,13 @@ struct pwm_generic {
 };
 extern struct pwm **pwms;
 #ifndef CONFIG_PWM_MULTI
-struct pwm *pwm_init(int32_t index);
+struct pwm *pwm_init(uint32_t index);
 int32_t pwm_deinit(struct pwm *pwm);
 
 int32_t pwm_setPeriod(struct pwm *pwm, uint64_t us);
 int32_t pwm_setDutyCycle(struct pwm *pwm, uint64_t us);
 #else
-inline struct pwm *pwm_init(int32_t index) {
+inline struct pwm *pwm_init(uint32_t index) {
 	struct pwm_generic *p = (struct pwm_generic *) pwms[index];
 	return p->ops->pwm_init(index);
 }
