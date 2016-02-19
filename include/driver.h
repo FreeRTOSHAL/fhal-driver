b03f2687906f0a2c07980902741859ed54f57d30
Werner2005 <webmaster@andy89.org>
2016-02-19 13:55:00 +0100

    move DEV defintion to driver include

--- include/driver.h
+++ include/driver.h
@@ -6,5 +6,14 @@
 	SYMBOL(_dev_##name); \
 	KEEP(*(.rodata.dev.##name)) \
 	SECTION_STOP(location)
+#define DEV_DEFAULT(location) DEV(hal, location) \
+	DEV(uart, location) \
+	DEV(timer, location) \
+	DEV(pwm, location) \
+	DEV(capture, location) \
+	DEV(spi, location) \
+	DEV(accel, location) \
+	DEV(gyro, location) \
+	DEV(adc, location)
 #endif
 #endif
