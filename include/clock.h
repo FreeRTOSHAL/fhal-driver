e7acc764266e0244be2548d50181dc66ba9444f8
Werner2005 <webmaster@andy89.org>
2016-03-03 17:12:26 +0100

    Add Dynamic Clock Speed Detection

--- include/clock.h
+++ include/clock.h
@@ -1,6 +1,7 @@
 #ifndef CLOCK_H_
 #define CLOCK_H_
 #include <stdint.h>
+#include <stdbool.h>
 /**
  * \defgroup CLOCK Clock Subsystem
  * \ingroup HAL
@@ -12,11 +13,30 @@
  * \todo Implement Clock Subsystem for better support for Power Mangement
  * \{
  */
+struct clock;
+/**
+ * Generic Clock Interface
+ */
+struct clock_generic {
+	bool init;
+};
 /**
  * Clock Init
  * \return Clock Instance or Null on error
  */
 struct clock *clock_init();
+/**
+ * Get CPU Clock Speed
+ * \param clk CLock Instance
+ * \return -1 on error >= 0 CPU Speed in Hz
+ */
+int64_t clock_getCPUSpeed(struct clock *clk);
+/**
+ * Get Periphery Clock Speed
+ * \param clk CLock Instance
+ * \return -1 on error >= 0 Periphery Speed in Hz
+ */
+int64_t clock_getPeripherySpeed(struct clock *clk);
 /**
  * Deinit Clock
  * \param clk CLock Instance
