083d3fde0b5fa21f145f3a973a558ee6ca62ec86
Andreas Werner <kernel@andy89.org>
2017-11-01 16:41:02 +0100

    backtrace: endless loop chceck

--- gcc/backtrace.c
+++ gcc/backtrace.c
@@ -2,9 +2,21 @@
 #include <unwind.h>
 _Unwind_Reason_Code trace_fcn(_Unwind_Context *ctx, void *d)
 {
+	static uintptr_t lastpc = 0x0;
+	static uint32_t counter = 0x0;
 	int *depth = (int*)d;
-	printf("\t#%d: program counter at 0x%x\n", *depth, _Unwind_GetIP(ctx));
+	uintptr_t pc = _Unwind_GetIP(ctx);
+	if (pc == lastpc) {
+		if (counter++ >= 1) {
+			printf("\t#%d: Endless Loop\n", *depth);
+			return _URC_END_OF_STACK;
+		}
+	} else {
+		counter = 0;
+	}
+	printf("\t#%d: program counter at 0x%x\n", *depth, pc);
 	(*depth)++;
+	lastpc = pc;
 	return _URC_NO_REASON;
 }
 void backtrace() {
