c0b0e211e979b685c92d94fdcf68cab403b689a5
Jonathan Klamroth <jonathan.klamroth@gmail.com>
2021-02-23 12:27:39 +0100

    add counter thread save option

--- counter/counter_generic.c
+++ counter/counter_generic.c
@@ -12,8 +12,18 @@ int32_t counter_genericInit(struct counter *t) {
 	if (hal_isInit(counter)) {
 		return COUNTER_ALREDY_INITED;
 	}
+#ifdef CONFIG_COUNTER_THREAD_SAVE
+	{
+		int32_t ret = hal_init(counter);
+		if (ret < 0) {
+			goto counter_genericInit_error0;
+		}
+	}
+#endif
 	counter->init = true;
 	return 0;
+counter_genericInit_error0:
+	return -1;
 }
 #ifdef CONFIG_COUNTER_MULTI
 struct counter *counter_init(uint32_t index, enum counter_mode mode);
