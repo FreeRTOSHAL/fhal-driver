402401990c58992ad8dd76d01d94d5019ef38739
Andreas Werner <kernel@andy89.org>
2021-10-08 13:49:41 +0200

    capture: remove Frama-C Spec and move it to mach

--- capture/capture_generic.c
+++ capture/capture_generic.c
@@ -6,19 +6,6 @@
 #include <capture.h>
 #define CAPTURE_PRV
 #include <capture_prv.h>
-/*@
-  requires \valid(t);
-  behavior isInit:
-    assumes ((struct capture_generic *) t)->init == true;
-    ensures \result == CAPTURE_ALREDY_INITED;
-    assigns \nothing;
-  behavior isNotInit:
-    assumes ((struct capture_generic *) t)->init == false;
-    ensures ((struct capture_generic *) t)->init == true;
-    ensures \result == 0;
-    assigns ((struct capture_generic *) t)->init;
-  disjoint behaviors;
- */
 int32_t capture_generic_init(struct capture *t) {
 	struct capture_generic *capture = (struct capture_generic *) t;
 	if (hal_isInit(capture)) {
